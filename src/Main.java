79821fff74fc1c3a19895eac9948a1c9b3f15581
albsva <albin.svanberg@elev.ga.ntig.se>
2023-10-23 14:18:09 +0200

    Nu fungerar min kod

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner tgb = new Scanner(System.in);
+        System.out.println("Vilket tal vill du veta gångertabellen till?");
+        int tal = tgb.nextInt();
 
-        // Press Skift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Skift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(tal + "*" + i + " = " +i * tal);
         }
     }
 }
